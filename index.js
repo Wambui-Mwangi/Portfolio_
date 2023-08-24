55516bdb6e3f627b3bcd127939a8b16f0e466071
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2023-08-24 22:26:39 +0300

    Portfolio

---
+++ index.js
@@ -0,0 +1,6 @@
+var type = new Typed('.hello', {
+    strings: ['println("Hello world!")',"print('Hello world!')","console.log('Hello World!')"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+})
